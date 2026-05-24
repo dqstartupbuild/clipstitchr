edf3d85ddc003ee6bc1bddcf633d64fde9a75340
DQ <dclouwork@gmail.com>
2026-05-24 13:32:24 -0400

    Update Convex generated API bindings

--- web/convex/_generated/api.d.ts
+++ web/convex/_generated/api.d.ts
@@ -29,6 +29,8 @@ import type * as swiprBackgrounds from "../swiprBackgrounds.js";
 import type * as validators_assetTags from "../validators/assetTags.js";
 import type * as validators_avatarWardrobeStyle from "../validators/avatarWardrobeStyle.js";
 import type * as validators_clipType from "../validators/clipType.js";
+import type * as validators_cliprCompositionStrategy from "../validators/cliprCompositionStrategy.js";
+import type * as validators_cliprContentType from "../validators/cliprContentType.js";
 import type * as validators_cliprDurationSeconds from "../validators/cliprDurationSeconds.js";
 import type * as validators_cliprJobStage from "../validators/cliprJobStage.js";
 import type * as validators_cliprJobStatus from "../validators/cliprJobStatus.js";
@@ -82,6 +84,8 @@ declare const fullApi: ApiFromModules<{
   "validators/assetTags": typeof validators_assetTags;
   "validators/avatarWardrobeStyle": typeof validators_avatarWardrobeStyle;
   "validators/clipType": typeof validators_clipType;
+  "validators/cliprCompositionStrategy": typeof validators_cliprCompositionStrategy;
+  "validators/cliprContentType": typeof validators_cliprContentType;
   "validators/cliprDurationSeconds": typeof validators_cliprDurationSeconds;
   "validators/cliprJobStage": typeof validators_cliprJobStage;
   "validators/cliprJobStatus": typeof validators_cliprJobStatus;
