3f02ce778af0bed90256e26f174f203b2a4a7000
DQ <dclouwork@gmail.com>
2026-06-23 23:33:23 -0400

    Force dynamic sitemap generation for updated entries

--- web/app/sitemap.ts
+++ web/app/sitemap.ts
@@ -1,6 +1,8 @@
 import type { MetadataRoute } from "next";
 import { getSitemapEntries } from "@/lib/getSitemapEntries";
 
+export const dynamic = "force-dynamic";
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   return await getSitemapEntries();
 }
